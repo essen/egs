753d3e843c3c3e201fc5d145cd3291a0e6516725
Loïc Hoguin <essen@dev-extend.eu>
2010-08-26 18:09:53 +0200

    psu_proto: Parse and then ignore command 0814.

--- src/psu/psu_proto.erl
+++ src/psu/psu_proto.erl
@@ -298,6 +298,22 @@ parse(Size, 16#0813, Channel, Data) ->
 	?ASSERT_EQ(VarJ, 16#ffffffff),
 	{npc_invite, NPCid};
 
+%% @doc This command should be safely ignored. Probably indicates that a mission area change was successful.
+parse(Size, 16#0814, Channel, Data) ->
+	<< _LID:16/little, VarA:16/little, VarB:32/little, VarC:32/little, VarD:32/little, VarE:32/little, VarF:32/little, VarG:32/little, VarH:32/little, VarI:32/little >> = Data,
+	?ASSERT_EQ(Size, 44),
+	?ASSERT_EQ(Channel, 2),
+	?ASSERT_EQ(VarA, 0),
+	?ASSERT_EQ(VarB, 0),
+	?ASSERT_EQ(VarC, 0),
+	?ASSERT_EQ(VarD, 0),
+	?ASSERT_EQ(VarE, 0),
+	?ASSERT_EQ(VarF, 0),
+	?ASSERT_EQ(VarG, 0),
+	?ASSERT_EQ(VarH, 0),
+	?ASSERT_EQ(VarI, 0),
+	ignore;
+
 %% @doc This command should be safely ignored. Probably indicates that a non-mission area change was successful.
 parse(Size, 16#0815, Channel, Data) ->
 	<< _LID:16/little, VarA:16/little, VarB:32/little, VarC:32/little, VarD:32/little, VarE:32/little, VarF:32/little, VarG:32/little, VarH:32/little, VarI:32/little >> = Data,
