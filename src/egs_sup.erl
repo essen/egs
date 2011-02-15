8f069e72d8ad7935517160507d61c96d2ec7746d
Loïc Hoguin <essen@dev-extend.eu>
2011-02-15 02:46:36 +0100

    egs_sup: Delete the useless upgrade function.

--- src/egs_sup.erl
+++ src/egs_sup.erl
@@ -20,28 +20,12 @@
 -module(egs_sup).
 -behaviour(supervisor).
 -export([init/1]). %% Supervisor callbacks.
--export([start_link/0, upgrade/0]). %% Other functions.
+-export([start_link/0]). %% Other functions.
 
 %% @spec start_link() -> ServerRet
 %% @doc API for starting the supervisor.
 start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
-%% @spec upgrade() -> ok
-%% @doc Add processes if necessary.
-upgrade() ->
-	{ok, {_, Specs}} = init([]),
-	Old = sets:from_list(
-		[Name || {Name, _, _, _} <- supervisor:which_children(?MODULE)]),
-	New = sets:from_list([Name || {Name, _, _, _, _, _} <- Specs]),
-	Kill = sets:subtract(Old, New),
-	sets:fold(fun (Id, ok) ->
-		supervisor:terminate_child(?MODULE, Id),
-		supervisor:delete_child(?MODULE, Id),
-		ok
-	end, ok, Kill),
-	[supervisor:start_child(?MODULE, Spec) || Spec <- Specs],
-	ok.
 
 %% @spec init([]) -> SupervisorTree
 %% @doc supervisor callback.
